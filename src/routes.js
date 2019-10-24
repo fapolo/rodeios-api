ef5d8db4682b11fb8bee490f889ba9df09b58e4a
Fabricio Polo <fabricio.polo@gmail.com>
2019-10-24 11:32:13 -0300

    add comments sobre rotas adicionais

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,8 @@ routes.get("/entidades", entidadeController.index);
 routes.post("/entidades", entidadeController.store);
 routes.get("/entidades/:entidade_id", entidadeController.show);
 routes.put("/entidades/:entidade_id", entidadeController.update);
+//delete entidade não deve ser permitido. Entidade é um elemento dos rodeios e dos resultados,
+//só existem no database por causa das participações nos rodeios
 
 routes.get("/rodeios", rodeioController.index);
 routes.post("/rodeios", rodeioController.store);
@@ -17,5 +19,7 @@ routes.put("/rodeios/:rodeio_id", rodeioController.update);
 routes.delete("/rodeios/:rodeio_id", rodeioController.delete);
 
 routes.post("/resultados/", resultadoController.store);
+//estudar update resultado ao liberar o update de rodeio no frontend, talvez endpoint dividido
+//para dados do rodeio em si (put rodeio) e aí criar (put resultado) para atualizar dados do resultado
 
 module.exports = routes;
